88a03a307453757d2126ba128c13513934c652b4
Zack <66165382+yoloyoyo@users.noreply.github.com>
2021-05-11 18:29:03 +0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "%_b24O(,TA":
+            case "Poopman":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . e e . . . . . . . 
+. . . . . . e e e . . . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . e e e e e e e e . . . . 
+. . . . e e e e e e e e e . . . 
+. . . e e e e e f e f e e e . . 
+. . . e e e e e e e e e e e . . 
+. . . e e e e e e 3 e e e e . . 
+. f . . e e e e e e e e e . f . 
+f f f f f e e e e e e e f f f f 
+. f . . . . . f . . f . . . f . 
+. . . . . . . f . . f . . . . . 
+. . . . . . . f . . f . . . . . 
+. . . . . . . f f . f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
